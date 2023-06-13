86de9788ef130dd809cac915ed6bd4ca48ef9d51
Murugan <vijayabharathi.murugan2@ab-inbev.com>
2023-06-14 01:10:05 +0530

    changes in main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "API active"}
+
 @app.get("/initiate_bot/{game_id} {player_id} {player_count}")
 async def initiate_bot(game_id : str, player_id : int, player_count : int):
     global bot
-    bot = LitBot(0,0,2,6,None,0)
+    bot = LitBot(game_id, player_id, 2, player_count, None, 0)
     return None
     
 
